88d00e576bebf0f5105984770cae0b7e6bf69e59
BoatNomad <133583026+BoatNomad@users.noreply.github.com>
2023-06-03 07:52:12 +0200

    Update CryptoWebScrapper.java

--- DynamicWebScrapper/src/main/java/org/example/CryptoWebScrapper.java
+++ DynamicWebScrapper/src/main/java/org/example/CryptoWebScrapper.java
@@ -3,6 +3,7 @@ package org.example;
 
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
 import data.CryptoData;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -11,58 +12,81 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 
 /**
- * Dynamic data webscrapper
+ * Static data webscrapper
  */
 public class CryptoWebScrapper {
     public static double price;
     public static String currency;
     public static String time;
+    static Scanner scanner = new Scanner(System.in);
+
+    public static String url = "https://www.plus500.com/pl/Instruments/BTCUSD?id=134463&tags=g_sr%2B16450615089_cpi%2BPolandSearchDSA_cp%2B133461103385_agi%2BGeneral.DSA_agn%2B_ks%2Bdsa-19959388920_tid%2B_mt%2Bc_de%2Bg_nt%2B_ext%2B9067404_loc%2BUURL&%D7%900";
 
 
     public static void main(String[] args) {
+        //mongod --dbpath C:\Users\pawel\Documents\Projekty_Java\Data
+        WebDriver driver = browserSetup();
         date();
-        fetchDataCurrency();
-        fetchDataPrice();
+        fetchDataCurrency(driver);
+        fetchDataPrice(driver);
 
-        System.err.println(date() + " " + fetchDataCurrency() + ": " + fetchDataPrice());
+        System.err.println(date() + " " + fetchDataCurrency(driver) + ": " + fetchDataPrice(driver));
         CryptoData.dbCollecting(price, currency, time);
     }
 
-    public static String fetchDataCurrency() {
-        WebDriverManager.chromedriver().setup();//Using the method setup from the WebDriver library
 
-        ChromeOptions options = new ChromeOptions(); //creating new object options
-        options.addArguments("--headless=new");// Without opening the browser
-        options.setBinary("C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"); //using method for options object
+    public static WebDriver browserSetup() {
+        System.out.println("Please select your browser by choosing the number: ");
+        System.out.println("1.Edge");
+        System.out.println("2.Chrome");
+        System.out.println("3.Firefox");
+        int select = scanner.nextInt();
 
-        WebDriver driver = new ChromeDriver(options); //creating new object driver
-        driver.get("https://www.plus500.com/pl/Instruments/BTCUSD?id=134463&tags=g_sr%2B16450615089_cpi%2BPolandSearchDSA_cp%2B133461103385_agi%2BGeneral.DSA_agn%2B_ks%2Bdsa-19959388920_tid%2B_mt%2Bc_de%2Bg_nt%2B_ext%2B9067404_loc%2BUURL&%D7%900");
+        switch (select) {
+            case 1:
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--headless=new");
+                WebDriver edgeDriver = new EdgeDriver(edgeOptions);
+                edgeDriver.get(url);
+                return edgeDriver;
+            case 2:
+                WebDriverManager.chromedriver().setup();//Using the method setup from the WebDriver library for Chromium browsers
+                ChromeOptions options = new ChromeOptions(); //creating new object options
+                options.addArguments("--headless=new");// Without opening the browser
+                options.setBinary("C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"); //using method for options object
+                WebDriver chromeDriver = new ChromeDriver(options); //creating new object driver
+                chromeDriver.get(url);
+                return chromeDriver;
+            case 3:
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--headless=new");
+                WebDriver firefoxDriver = new FirefoxDriver(firefoxOptions);
+                firefoxDriver.get(url);
+                return firefoxDriver;
 
+        }
+        return null;
+    }
+
+    public static String fetchDataCurrency(WebDriver driver) {
         WebElement element = driver.findElement(By.cssSelector("span.inst-name")); //creating new variable element1
-
         currency = element.getText();
-
         driver.quit();
         return currency;
     }
 
-    public static double fetchDataPrice() {
-        WebDriverManager.chromedriver().setup();//Using the method setup from the WebDriver library
-
-        ChromeOptions options = new ChromeOptions(); //creating new object options
-        options.addArguments("--headless=new");// Without opening the browser
-        options.setBinary("C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"); //using method for options object
-
-        WebDriver driver = new ChromeDriver(options); //creating new object driver
-        driver.get("https://www.plus500.com/pl/Instruments/BTCUSD?id=134463&tags=g_sr%2B16450615089_cpi%2BPolandSearchDSA_cp%2B133461103385_agi%2BGeneral.DSA_agn%2B_ks%2Bdsa-19959388920_tid%2B_mt%2Bc_de%2Bg_nt%2B_ext%2B9067404_loc%2BUURL&%D7%900");
-
+    public static double fetchDataPrice(WebDriver driver) {
         WebElement element = driver.findElement(By.cssSelector("span.rate")); //creating new variable element
-
         price = Double.parseDouble(element.getText());
-
         driver.quit();
         return price;
     }
@@ -76,4 +100,15 @@ public class CryptoWebScrapper {
 
     }
 
+    public static String getCurrency() {
+        return currency;
+    }
+
+    public static double getPrice() {
+        return price;
+    }
+
+    public static String getDate() {
+        return time;
+    }
 }
